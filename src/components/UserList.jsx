15628b6dd9d544a4f850a091c0e2f1444161f4d5
Igor <kisel.igor@yahoo.com>
2023-11-08 19:43:09 +0300

    Env fix

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -28,15 +28,16 @@ function UserList() {
         sortOrder: sortConfig.direction,
       }
 
+      //  {
+      //           method: 'GET',
+      //           headers: {
+      //             'Content-Type': 'application/json',
+      //           },
+      //           body: JSON.stringify(requestData),
+      //         }
+
       fetch(
-        `${process.env.REACT_APP_API_URL}/users?criteria=${searchCriteria}&value=${searchValue}&sortKey=${query.sortKey}&sortOrder=${query.sortOrder}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(requestData),
-        }
+        `${process.env.REACT_APP_API_URL}/users?criteria=${searchCriteria}&value=${searchValue}&sortKey=${query.sortKey}&sortOrder=${query.sortOrder}`
       )
         .then((response) => response.json())
         .then((data) => {
